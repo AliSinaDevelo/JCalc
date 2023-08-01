0c1ecc4048cb3ba772a907be3bc0fac0470231e0
AliSina Karimi <100498538+AliSinaDevelo@users.noreply.github.com>
2023-08-02 01:16:29 +0200

    main.js fix

--- scripts/main.js
+++ scripts/main.js
@@ -12,18 +12,18 @@ function multiply(a, b) {
 
 function divide(a, b) {
     if (b === 0) {
-        dispalyErrorMessage("Cannot divide by Zero!");
+        displayErrorMessage("Cannot divide by Zero!");
         return;
     }
     return a / b;
 }
 
 function operate(operator, a, b) {
-    switch(operator) {
+    switch (operator) {
         case '+':
             return add(a, b);
         case '-':
-            return subtract(a, b)    
+            return subtract(a, b);
         case '*':
             return multiply(a, b);
         case '/':
@@ -37,64 +37,63 @@ function operate(operator, a, b) {
 let displayValue = '';
 
 function updateDisplay(value) {
-  displayValue += value;
-  document.getElementById('display').value = displayValue;
+    displayValue += value;
+    document.getElementById('display').value = displayValue;
 }
 
 function clearDisplay() {
-  displayValue = '';
-  document.getElementById('display').value = '';
+    displayValue = '';
+    document.getElementById('display').value = '';
 }
 
 let firstNumber = null;
 let operator = null;
 
 document.querySelectorAll('.number').forEach(button => {
-  button.addEventListener('click', () => {
-    updateDisplay(button.textContent);
-  });
+    button.addEventListener('click', () => {
+        updateDisplay(button.textContent);
+    });
 });
 
 document.querySelectorAll('.operator').forEach(button => {
-  button.addEventListener('click', () => {
-    if (firstNumber === null) {
-      firstNumber = parseFloat(displayValue);
-      operator = button.textContent;
-      clearDisplay();
-    } else {
-      const secondNumber = parseFloat(displayValue);
-      const result = operate(operator, firstNumber, secondNumber);
-      if (result !== null) {
-        firstNumber = result;
-        operator = button.textContent;
-        document.getElementById('display').value = result;
-      }
-      clearDisplay();
-    }
-  });
+    button.addEventListener('click', () => {
+        if (firstNumber === null) {
+            firstNumber = parseFloat(displayValue);
+            operator = button.textContent;
+            clearDisplay();
+        } else {
+            const secondNumber = parseFloat(displayValue);
+            const result = operate(operator, firstNumber, secondNumber);
+            if (result !== null) {
+                firstNumber = result;
+                operator = button.textContent;
+                document.getElementById('display').value = result;
+            }
+            clearDisplay();
+        }
+    });
 });
 
 document.getElementById('equals').addEventListener('click', () => {
-  if (firstNumber !== null && operator !== null) {
-    const secondNumber = parseFloat(displayValue);
-    const result = operate(operator, firstNumber, secondNumber);
-    if (result !== null) {
-      document.getElementById('display').value = result;
-      firstNumber = result;
-      operator = null;
+    if (firstNumber !== null && operator !== null) {
+        const secondNumber = parseFloat(displayValue);
+        const result = operate(operator, firstNumber, secondNumber);
+        if (result !== null) {
+            document.getElementById('display').value = result;
+            firstNumber = result;
+            operator = null;
+        }
+        clearDisplay();
     }
-    clearDisplay();
-  }
 });
 
 document.getElementById('clear').addEventListener('click', () => {
-  firstNumber = null;
-  operator = null;
-  clearDisplay();
+    firstNumber = null;
+    operator = null;
+    clearDisplay();
 });
 
 function displayErrorMessage(message) {
     document.getElementById('display').value = message;
     setTimeout(clearDisplay, 2000);
 }
-  
